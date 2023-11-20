30db2dc3d9fb46474ba8d0c9feab30ad23d16b7c
Sita Ram Thing <sitaramthing667@gmail.com>
2023-11-20 13:42:48 +0545

    create the new sample FDA application which is for FYP project helps then create the splash screen and Walkthrough Screen is completed

---
+++ Projects/FDA/app/src/main/java/com/donation/fda/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.donation.fda
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.donation.fda.presentation.ui.IntroSliderScreen
+import com.donation.fda.ui.theme.FDATheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FDATheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    IntroSliderScreen()
+                }
+            }
+        }
+    }
+}
---
+++ Projects/FDA/app/src/main/java/com/donation/fda/presentation/ui/WelcomeScreen.kt
@@ -0,0 +1,2 @@
+package com.donation.fda.presentation.ui
+
