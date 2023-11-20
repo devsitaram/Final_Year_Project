0d786770002aae6a1602ef44975bd53021276120
Sita Ram Thing <sitaramthing667@gmail.com>
2023-11-20 17:14:39 +0545

    create the welcome page ButtonSheet bar

--- Projects/FDA/app/src/main/java/com/donation/fda/presentation/ui/WelcomeScreen.kt
+++ Projects/FDA/app/src/main/java/com/donation/fda/presentation/ui/WelcomeScreen.kt
@@ -1,37 +1,205 @@
 package com.donation.fda.presentation.ui
 
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.BottomSheetScaffold
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.rememberBottomSheetScaffoldState
+import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.ShapeDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import coil.compose.rememberAsyncImagePainter
 import com.donation.fda.presentation.components.ButtonView
+import com.donation.fda.presentation.components.InputTextFieldView
 import com.donation.fda.presentation.components.TextView
 import com.donation.fda.theme.primaryColor
 import com.donation.fda.theme.white
 import com.record.fda.R
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun WelcomeViewScreen() {
+
+    val scope = rememberCoroutineScope()
+    val scaffoldState = rememberBottomSheetScaffoldState()
+
+    // checkbox action
+    val signUponClick: () -> Unit = {
+        scope.launch {
+            if (scaffoldState.bottomSheetState.isExpanded) {
+                scaffoldState.bottomSheetState.collapse()
+            } else {
+                scaffoldState.bottomSheetState.expand()
+            }
+        }
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(white)
+    ) {
+        BottomSheetScaffold(
+            scaffoldState = scaffoldState,
+            sheetPeekHeight = 0.dp,
+            sheetShape = ShapeDefaults.ExtraLarge,
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(alignment = Alignment.CenterHorizontally),
+            sheetContent = {
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color.LightGray)
+                ) {
+                    CountryList(
+                        onClickAction = {
+                            scope.launch {
+                                if (scaffoldState.bottomSheetState.isExpanded) {
+                                    scaffoldState.bottomSheetState.collapse()
+                                } else {
+                                    scaffoldState.bottomSheetState.expand()
+                                }
+                            }
+                        }
+                    )
+                }
+            }
+        ) { innerPadding ->
+            // this is the screen
+            Column(
+                modifier = Modifier
+                    .background(Color.White)
+                    .fillMaxSize()
+                    .padding(innerPadding)
+                    .verticalScroll(rememberScrollState()),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                // Action button
+                WelcomeViewScreen(
+                    signInOnClick = { },
+                    signUponClick = { signUponClick() }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun CountryList(onClickAction: () -> Unit = {}) {
+    var context = LocalContext.current
+    var userList = listOf(
+        UserList(
+            logo = painterResource(id = R.mipmap.img_ngo_logo),
+            userType = "NOGs"
+        ),
+        UserList(
+            logo = painterResource(id = R.mipmap.img_volunteer),
+            userType = "Volunteers"
+        ),
+        UserList(
+            logo = painterResource(id = R.mipmap.img_donor),
+            userType = "Donors"
+        ),
+        UserList(
+            logo = painterResource(id = R.mipmap.img_farmer),
+            userType = "Farmers"
+        )
+    )
+
+//    var textValues by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // Show button to hide bottom sheet
+        TextView(
+            text = "Register your details",
+            style = MaterialTheme.typography.h5,
+            color = Color.DarkGray,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth().padding(bottom = 10.dp)
+        )
+
+        // Display user list using LazyColumn
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            modifier = Modifier
+        ) {
+            this.items(userList) { user ->
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable {
+                            onClickAction()
+                        }
+                        .padding(vertical = 10.dp, horizontal = 15.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Card(modifier = Modifier.wrapContentSize().size(140.dp), shape = ShapeDefaults.Large) {
+                        Image(
+                            painter = user.logo,
+                            contentDescription = null,
+                            modifier = Modifier.fillMaxSize()
+                        )
+                    }
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(text = user.userType)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun WelcomeViewScreen(signInOnClick: () -> Unit, signUponClick: () -> Unit) {
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -39,7 +207,7 @@ fun WelcomeViewScreen() {
             .padding(8.dp), verticalArrangement = Arrangement.Top
     ) {
         Image(
-            painter = rememberAsyncImagePainter(model = R.mipmap.img_profile),
+            painter = painterResource(R.mipmap.img_profile),
             contentDescription = null,
             modifier = Modifier
                 .fillMaxWidth()
@@ -73,7 +241,7 @@ fun WelcomeViewScreen() {
         ) {
 
             ButtonView(
-                onClick = { /*TODO*/ }, text = "Sign In",
+                onClick = { signInOnClick() }, text = "Sign In",
                 colors = ButtonDefaults.buttonColors(
                     containerColor = primaryColor,
                 ),
@@ -84,7 +252,7 @@ fun WelcomeViewScreen() {
             )
             Spacer(modifier = Modifier.padding(5.dp))
             ButtonView(
-                onClick = { /*TODO*/ },
+                onClick = { signUponClick() },
                 text = "Sign Up",
                 colors = ButtonDefaults.buttonColors(
                     containerColor = Color.Unspecified,
@@ -98,3 +266,8 @@ fun WelcomeViewScreen() {
         }
     }
 }
+
+data class UserList(
+    val logo: Painter,
+    val userType: String
+)
